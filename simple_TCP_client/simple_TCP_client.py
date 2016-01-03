1cd420aef95cfafc2228b9b245713590ae59f2bf
caimaoy <caimaoy.gmail.com>
2016-01-03 22:57:17 +0800

    close TCP client

--- simple_TCP_client/simple_TCP_client.py
+++ simple_TCP_client/simple_TCP_client.py
@@ -8,10 +8,15 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 client.send("GET / HTTP/1.1\r\nHost: caimaoy.com\r\n\r\n")
-while True:
-    response = client.recv(4096)
-    if not response:
-        client.sent('ACK!')
-        client.close()
+recv_len = 1
+response = ''
+while recv_len:
+    data = client.recv(4096)
+    recv_len = len(data)
+    response += data
+    if recv_len < 4096:
+        print '[*] len = %s' % recv_len
+        print repr(data)
+    if response.endswith('</html>'):
         break
-    print response
+print response
